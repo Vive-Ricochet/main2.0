5a4b77440bf10f1a30bb3b9d0ef22766379c3093
tmcqueen723 <tmcqueen@ucsc.edu>
2016-02-12 23:41:17 -0800

    Alpha 2.0

---
+++ Viverichochet0.1/Assets/Scripts/ItemSpawning.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemSpawning : MonoBehaviour {
+    bool drop;
+	int timer = 400;
+    [SerializeField] public List<GameObject> GameObjs = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+        drop = true;
+	}
+
+    //Vector3 position = new Vector3( transform.position.x + Random.Range(-10.0F, 10.0F), 0, Transform.position.z + Random.Range(-10.0F, 10.0F));
+
+    // Update is called once per frame
+    void Update () {
+		if (drop == true && timer == 0) {
+			foreach (GameObject Thing in GameObjs) {
+				Vector3 randomDir = new Vector3 (Random.Range (-50.0f, 50.0f), 0, Random.Range (-50.0f, 50.0f));
+				Instantiate (Thing, transform.position + randomDir, transform.rotation);
+				timer = 400;
+			}
+		} else if (timer != 0) {
+			timer -= 1;
+		}
+	}
+}
